ca4fffa00e1ff1c33d8bc4b6667773183cfdcadd
nmineiromainhub <nmineiro@mainhub.pt>
2024-01-09 17:30:52 +0000

    solvemefirst

---
+++ HackerRank/SolveMeFirst/SolveMeFirstLibrary/SolveMeFirstLibrary.cs
@@ -0,0 +1,13 @@
+﻿namespace SolveMeFirstLibrary;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+public class SolveMeFirstLibrary
+{
+
+    public static int SolveMeFirst(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ HackerRank/SolveMeFirst/SolveMeFirstLibraryTests/SolveMeFirstLibraryTests.cs
@@ -0,0 +1,23 @@
+using SolveMeFirstLibrary;
+
+namespace SolveMeFirstLibraryTests
+{
+    public class SolveMeFirstLibraryTests
+    {
+
+        [Fact]
+        static void SolveMeFirstLibrary()
+        {
+
+            int value1 = 2;
+            int value2=3;
+            int sum = value1 + value2;
+            Assert.Equal(5,sum);
+        }
+    }
+}
+
+
+
+
+
